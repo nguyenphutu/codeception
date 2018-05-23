5ed209d3b8f3936c1aa89311335f7b561f6381f6
nguyen phu tu <nguyentudb1995@gmail.com>
2018-05-23 15:57:31 +0700

    aaaa

--- tests/acceptance/Test_Sample_LaravelCest.php
+++ tests/acceptance/Test_Sample_LaravelCest.php
@@ -21,4 +21,14 @@ class Test_Sample_LaravelCest
 		$I -> seeLink('Forge');
 		$I -> seeLink('GitHub');
     }
+	
+	public function clickLinkDocumentation(AcceptanceTester $I)
+    {
+		$I -> amOnPage('/');
+		$I->expect('Click link documentation anh check link not die.');
+		$I -> seeLink('Documentation');
+		$I -> click(['link' => 	'Documentation']);
+		$I -> dontSee('Sorry, the page you are looking for could not be found.');
+		$I -> seeInCurrentUrl('docs/');
+    }
 }
